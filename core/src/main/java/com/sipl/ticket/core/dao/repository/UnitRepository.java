022dacccd51881e3e4fe1ec50941593baca6d9a2
suraj.g <suraj.g@surajinformatics.in>
2025-12-30 17:15:51 +0530

    CRUD operations with proper pull.  @Valid added in controller

--- core/src/main/java/com/sipl/ticket/core/dao/repository/UnitRepository.java
+++ core/src/main/java/com/sipl/ticket/core/dao/repository/UnitRepository.java
@@ -2,12 +2,15 @@ package com.sipl.ticket.core.dao.repository;
 
 import com.sipl.ticket.core.dao.entity.Unit;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface UnitRepository extends JpaRepository<Unit, Long> {
+    @Query("From Unit u where u.unitId = :unitId AND u.isActive = true")
+    Optional<Unit> findActiveById(Long unitId);
 
-    List<Unit> findByIsActiveTrue();
 }
