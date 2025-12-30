b1521ac121084fa16d8bf34c0965aa7db89a597b
TusharMargale <88328301+TusharMargale@users.noreply.github.com>
2025-12-30 13:53:17 +0530

    search endpoint created

--- company/src/main/java/com/sipl/ticket/company/service/CompanyService.java
+++ company/src/main/java/com/sipl/ticket/company/service/CompanyService.java
@@ -2,10 +2,13 @@ package com.sipl.ticket.company.service;
 
 
 import com.sipl.ticket.core.dto.request.CompaniesRequestDto;
+import com.sipl.ticket.core.dto.request.CompanySearchRequestDto;
 import com.sipl.ticket.core.dto.response.ApiResponseDTO;
 import com.sipl.ticket.core.dto.response.CompanyDto;
 import com.sipl.ticket.core.dto.response.PagedResponse;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public interface CompanyService {
@@ -25,5 +28,9 @@ public interface CompanyService {
             Long companyId
     );
 
-    ApiResponseDTO<PagedResponse<CompanyDto>> getAllCompanies();
+    ApiResponseDTO<CompanyDto> getAllCompanies();
+
+    ApiResponseDTO<PagedResponse<CompanyDto>> searchCompanies(
+            CompanySearchRequestDto requestDto
+    );
 }
