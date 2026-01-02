dbb33444c587e665792e0eee505248ea5e7a9fa0
TusharMargale <88328301+TusharMargale@users.noreply.github.com>
2026-01-02 16:28:34 +0530

    save endpoint created with validation

--- task/src/main/java/com/sipl/ticket/task/service/TaskService.java
+++ task/src/main/java/com/sipl/ticket/task/service/TaskService.java
@@ -1,4 +1,17 @@
 package com.sipl.ticket.task.service;
 
+import com.sipl.ticket.core.dto.response.ApiResponseDTO;
+import com.sipl.ticket.core.dto.response.CombinedTaskResponseDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@Service
 public interface TaskService {
+
+    ApiResponseDTO<CombinedTaskResponseDto> addTask(
+            String taskRequestDto,
+            List<MultipartFile> files
+    );
 }
