159f35783911bce75c8600f9b6b6cf60ca591864
Prathamesh <shindeprathamesh9891@gmail.com>
2026-01-09 16:31:32 +0530

    remove tag id

--- core/src/main/java/com/sipl/ticket/core/dao/repository/TagsRepository.java
+++ core/src/main/java/com/sipl/ticket/core/dao/repository/TagsRepository.java
@@ -21,7 +21,6 @@ public interface TagsRepository extends JpaRepository<Tags, Long> {
             "SELECT t FROM Tags t " +
                     "WHERE t.isActive = true " +
                     "AND ( :query IS NULL OR :query = '' " +
-                    "      OR CAST(t.tagId AS string) LIKE CONCAT('%', :query, '%') " +
                     "      OR LOWER(t.tagName) LIKE CONCAT('%', LOWER(:query), '%') )"
     )
     Page<Tags> searchTags(
