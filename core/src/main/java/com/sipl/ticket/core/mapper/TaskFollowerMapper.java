5b3f3605fa5360d9b156897fbb139a9fabcb1d3d
TusharMargale <88328301+TusharMargale@users.noreply.github.com>
2026-02-02 12:01:11 +0530

    set some fields

--- core/src/main/java/com/sipl/ticket/core/mapper/TaskFollowerMapper.java
+++ core/src/main/java/com/sipl/ticket/core/mapper/TaskFollowerMapper.java
@@ -4,6 +4,7 @@ import com.sipl.ticket.core.dao.entity.TaskFollower;
 import com.sipl.ticket.core.dto.response.TaskFollowerCustomResponseDTO;
 import com.sipl.ticket.core.dto.response.TaskFollowerDto;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 import java.util.List;
 
@@ -24,5 +25,9 @@ public interface TaskFollowerMapper extends AuditEntityMapper{
     List<TaskFollowerDto> mapToDtoList(List<TaskFollower> entities);
 
 
+    @Mapping(source = "user.id", target = "userId")
+    @Mapping(source = "user.userName", target = "userName")
+    @Mapping(source = "task.taskId", target = "taskId")
+    @Mapping(source = "task.subject", target = "subject")
     TaskFollowerCustomResponseDTO toCustomDto(TaskFollower taskFollower);
 }
